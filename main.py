42d8710e18c2b3f25a7682e0c69c282f2e365d2d
csabri2 <csabri2@illinois.edu>
2021-09-24 21:59:32 -0500

    Modified given main code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from neo4j import GraphDatabase
 
-class HelloWorldExample:
+class pyghack:
 
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
@@ -8,19 +8,21 @@ class HelloWorldExample:
     def close(self):
         self.driver.close()
 
-    def print_greeting(self, message):
+    def add_event(self, name, start_time, end_time, type):
         with self.driver.session() as session:
-            greeting = session.write_transaction(self._create_and_return_greeting, message)
-            print(greeting)
+            session.write_transaction(self._create_and_return_event, name, start_time, end_time, type)
 
     @staticmethod
-    def _create_and_return_greeting(tx, message):
-        result = tx.run("CREATE (a:Greeting) "
-                        "SET a.message = $message "
-                        "RETURN a.message + ', from node ' + id(a)", message=message)
+    def _create_and_return_event(tx, name, start_time, end_time, type):
+        result = tx.run("CREATE (a:Event) "
+                        "SET a.name = $name, "
+                        "a.start_time = $start_time, "
+                        "a.end_time = $end_time, "
+                        "a.type = $type", name = name, start_time = start_time, end_time = end_time, type = type)
         return result.single()[0]
 
 
 if __name__ == "__main__":
-    greeter = HelloWorldExample("bolt://192.168.137.2:7687/", "neo4j", "1234")
-    greeter.close()
+    database = pyghack("bolt://192.168.137.2:7687/", "neo4j", "1234")
+    database.add_event("Study group", "1700", "1900", "study")
+    database.close()
