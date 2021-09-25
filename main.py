77334074570246785227c5ed6349963bd8275464
csabri2 <csabri2@illinois.edu>
2021-09-24 22:27:30 -0500

    Rewrote add_event function

--- main.py
+++ main.py
@@ -8,21 +8,25 @@ class pyghack:
     def close(self):
         self.driver.close()
 
+    # def add_event(self, user, name, start_time, end_time, type):
+    #     with self.driver.session() as session:
+    #         session.write_transaction(self._create_and_return_event, user, name, start_time, end_time, type)
+
+    # @staticmethod
+    # def _create_and_return_event(tx, user, name, start_time, end_time, type):
+    #     result = tx.run("CREATE (a:Event) "
+    #                     "SET a.user = $user, "
+    #                     "a.name = $name, "
+    #                     "a.start_time = $start_time, "
+    #                     "a.end_time = $end_time, "
+    #                     "a.type = $type", user = user, name = name, start_time = start_time, end_time = end_time, type = type)
+    #     return result.single()[0]
+
     def add_event(self, user, name, start_time, end_time, type):
         with self.driver.session() as session:
-            session.write_transaction(self._create_and_return_event, user, name, start_time, end_time, type)
-
-    @staticmethod
-    def _create_and_return_event(tx, user, name, start_time, end_time, type):
-        result = tx.run("CREATE (a:Event) "
-                        "SET a.user = $user, "
-                        "a.name = $name, "
-                        "a.start_time = $start_time, "
-                        "a.end_time = $end_time, "
-                        "a.type = $type", user = user, name = name, start_time = start_time, end_time = end_time, type = type)
-        return result.single()[0]
+            session.run("CREATE (a:Event {user: $user, name: $name, start_time: $start_time, end_time: $end_time, type: $type})", user=user, name=name, start_time=start_time, end_time=end_time, type=type)
     
-    # def _create_and_return_user(tx, )
+    # def _create_and_return_user(tx, name, )
 
 
 if __name__ == "__main__":
